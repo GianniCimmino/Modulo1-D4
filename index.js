b84cac064ec0dd2a46f2a6d8fb19918f7b63ef43
Gianni <giannicimmino1996@gmail.com>
2023-07-03 11:53:20 +0200

    final commit

--- index.js
+++ index.js
@@ -3,16 +3,19 @@
 /*
 Oggi il tuo compito è creare la logica per un sito di e-commerce che deve supportare sconti extra per utenti speciali.
 A partire da una lista di prezzi, un utente e un costo di spedizione l'algoritmo deve determinare il costo totale del carrello.
-ATTENZIONE! Gli argomenti di questa settimana sono cruciali per lo svolgimento di un lavoro di un developer (il 90% del dati che maneggerai saranno array di oggetti!!) quindi 
+ATTENZIONE! Gli argomenti di questa settimana sono cruciali per lo svolgimento di un lavoro di un developer (il 90% del dati che 
+  maneggerai saranno array di oggetti!!) quindi 
 assicurati di COMPRENDERE la logica. Se ti trovi in difficolta', scrivi ad un membro del teaching staff! :) 
 
-Se l'utente ha la proprietà "isAmbassador" con valore true, il carrello deve venire scontato del 30%, PRIMA di calcolare la spedizione (anche gli utenti speciali 
-  pagano le spedizioni).
+Se l'utente ha la proprietà "isAmbassador" con valore true, il carrello deve venire scontato del 30%, PRIMA di calcolare la spedizione 
+(anche gli utenti speciali pagano le spedizioni).
 Se l'utente ha la proprietà "isAmbassador" con valore false, il carrello NON deve venire scontato.
-In entrambi i casi, la spedizione è gratuita per ogni carrello con costo superiore a 100. Altrimenti, aggiungi il valore fornito per coprire il costo della spedizione.
+In entrambi i casi, la spedizione è gratuita per ogni carrello con costo superiore a 100. Altrimenti, aggiungi il valore fornito 
+per coprire il costo della spedizione.
 
 In basso troverai degli esempi di utenti, una lista prezzi e un costo fisso di spedizione.
-Crea un array di utenti (usando .push) e stampa, per ogni utente (quindi con un loop) la frase "NOMEUTENTE COGNOMEUTENTE e' / non e' un ambassador" basandoti sui dati 
+Crea un array di utenti (usando .push) e stampa, per ogni utente (quindi con un loop) la frase "NOMEUTENTE COGNOMEUTENTE e' / non e' 
+un ambassador" basandoti sui dati 
 contenuti nell'oggetto. 
 ES. L'utente Marco Rossi e' un ambassador, quindi la frase dovrebbe essere "Marco Rossi e' un ambassador"
 Infine, crea un SECONDO array in cui inserirai SOLO gli ambassador.
@@ -46,48 +49,37 @@ ambassador.push(marco)
 
 for(let i = 0; i < client.length; i++)
 {
-  console.log("nome utente: " + client[i].name + " cognome utente: " + client[i].lastName + " e' ambasciatore " + client[i].isAmbassador)
+  if(client[i].isAmbassador === true)
+  console.log("nome utente: " + client[i].name + " cognome utente: " + client[i].lastName + " E' ambasciatore " + client[i].isAmbassador)
+  else{
+    console.log("nome utente: " + client[i].name + " cognome utente: " + client[i].lastName + " Non e' ambasciatore " + client[i].isAmbassador)
+  }
 }
 
-
-
-
-
-
-// const listaPersone = [];
-
-// listaPersone.push (marco, paul, amy);
-// console.log(listaPersone)
-
-// for (let i=0;i<listaPersone.lenght;i++)
-// {
-//   console.log("Nome utente : " + listaPersone[i].name + " Cognome utente : " + listaPersone[i].lastName + " E ambassador : " + listaPersone[i].isAmbassador)
-//  }
-
-// if (listaPersone === true)
-//   {
-//     console.log(amy.name, amy.lastName + "e' un ambassador")
-//   }
-//   else 
-//   {
-//     console.log(listaPersone + "non e' un ambassador")
-//   }
-
-
-// for(let i=0;i<listaPersone.length;i++)
-// {   
-//   console.log("Nome utente : " + listaPersone[i].name + " Cognome utente : " + listaPersone[i].lastName + " E ambassador : " + listaPersone[i].isAmbassador)
-//  }
-  
-
-
-
-
-
-
-
-
-const prices = [34, 5, 2]
+const prices = [100, 5, 2]
 const shippingCost = 50
 let utenteCheEffettuaLAcquisto = amy //cambia il valore qui per provare se il tuo algoritmo funziona!
+
+let sommaCarrello = 0
+for(let i = 0; i < prices.length; i++)
+{
+  let prezzo = prices[i]
+  sommaCarrello += prezzo;
+}
+
+if(utenteCheEffettuaLAcquisto.isAmbassador) { sommaCarrello = (sommaCarrello * 0,7)}
+
+if(sommaCarrello <= 100) {sommaCarrello += shippingCost}
+
+console.log(utenteCheEffettuaLAcquisto)
+
+
+
+
+
+
+
+
+
+
 
